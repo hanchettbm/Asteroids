bb6a96988a059500ed261acb775f0317922a7e15
Baden <hanchettbm@gmail.com>
2020-06-27 12:33:19 -0600

    Initial Commit

---
+++ ASTEROIDS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASTEROIDS
+//
+//  Created by Baden Hanchett on 6/27/20.
+//  Copyright © 2020 Baden Hanchett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
